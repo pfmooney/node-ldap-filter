f13993b39163848ab27b607b945d9ba6a8754fbc
Patrick Mooney <patrick.f.mooney@gmail.com>
2014-10-17 13:52:54 -0500

    Improve API for custom match functions

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -6,48 +6,64 @@ var test = require('tape').test;
 
 ///--- Globals
 
-var helpers;
+var lib;
+var testValues;
+var getAttrValue;
 
 
 ///--- Tests
 
 test('load library', function (t) {
-  helpers = require('../lib/helpers');
-  t.ok(helpers);
+  lib = require('../lib/index');
+  t.ok(lib);
+  t.ok(testValues = lib.testValues);
+  t.ok(getAttrValue = lib.getAttrValue);
   t.end();
 });
 
 
-test('multiTest array', function (t) {
+test('testValues array', function (t) {
   var rule = function (item) {
     return (item == 3);
   };
-  t.ok(helpers.multiTest(rule, [1, 2, 3]));
-  t.ok(!helpers.multiTest(rule, [1, 2]));
+  t.ok(testValues(rule, [1, 2, 3]));
+  t.ok(!testValues(rule, [1, 2]));
   t.end();
 });
 
 
-test('multiTest value', function (t) {
+test('testValues value', function (t) {
   var rule = function (item) {
     return (item == 3);
   };
-  t.ok(helpers.multiTest(rule, 3));
-  t.ok(!helpers.multiTest(rule, 1));
+  t.ok(testValues(rule, 3));
+  t.ok(!testValues(rule, 1));
   t.end();
 });
 
 
-test('getAttrCaseless exact match', function (t) {
-  var f = helpers.getAttrCaseless;
+test('testValues allMatch', function (t) {
+  var rule = function (item) {
+    return (item == 3);
+  };
+  t.ok(testValues(rule, 3, true));
+  t.ok(!testValues(rule, 1, true));
+  t.ok(testValues(rule, [3, 3, 3], true));
+  t.ok(!testValues(rule, [3, 2, 1], true));
+  t.end();
+});
+
+
+test('getAttrValue exact match', function (t) {
+  var f = getAttrValue;
   t.equal(f({attr: 'testval'}, 'attr'), 'testval');
-  t.equal(f({attr: 'testval'}, 'missing'), null);
+  t.equal(f({attr: 'testval'}, 'missing'), undefined);
   t.end();
 });
 
 
-test('getAttrCaseless insensitive match', function (t) {
-  var f = helpers.getAttrCaseless;
+test('getAttrValue insensitive match', function (t) {
+  var f = getAttrValue;
   var data = {
     lower: 'lower',
     UPPER: 'upper',
@@ -56,6 +72,23 @@ test('getAttrCaseless insensitive match', function (t) {
   t.equal(f(data, 'lower'), 'lower');
   t.equal(f(data, 'upper'), 'upper');
   t.equal(f(data, 'mixed'), 'mixed');
-  t.equal(f(data, 'missing'), null);
+  t.equal(f(data, 'missing'), undefined);
+  t.end();
+});
+
+
+test('getAttrValue strict match', function (t) {
+  var f = getAttrValue;
+  var data = {
+    lower: 'lower',
+    UPPER: 'upper',
+    MiXeD: 'mixed'
+  };
+  t.equal(f(data, 'lower', true), 'lower');
+  t.equal(f(data, 'upper', true), undefined);
+  t.equal(f(data, 'UPPER', true), 'upper');
+  t.equal(f(data, 'mixed', true), undefined);
+  t.equal(f(data, 'MiXeD', true), 'mixed');
+  t.equal(f(data, 'missing', true), undefined);
   t.end();
 });
